da4d455cc39f9e3167a8a83bc190f4496d746619
Kev Comedia <kevcomedia@gmail.com>
2017-10-08 16:14:01 +0800

    Return timestamp from unix time input
    For now, I'm returning the natural date naively, using US dates. It'd be
    nice if the natural date is i18n-ized.

--- server.js
+++ server.js
@@ -8,6 +8,25 @@ app.get('/', function(req, res) {
   res.send('Timestamp microservice');
 });
 
+app.get('/:date', function(req, res) {
+  const {date} = req.params;
+  const timestamp = {};
+
+  // Unix time
+  if (!Number.isNaN(+date)) {
+    const natural = new Date(date * 1000);
+    const dateOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    };
+    timestamp.unix = +date;
+    timestamp.natural = natural.toLocaleDateString('en-US', dateOptions);
+  }
+
+  res.json(timestamp);
+});
+
 app.listen(port, function() {
   console.log("App is alive at port " + port);
 });
