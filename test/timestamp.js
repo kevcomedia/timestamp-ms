442096f998a3e86a9b48e27e519689096941e24e
Kev Comedia <kevcomedia@gmail.com>
2017-10-09 15:38:22 +0800

    Write tests for different inputs

--- test/timestamp.js
+++ test/timestamp.js
@@ -56,4 +56,82 @@ describe('Timestamp', () => {
         });
     });
   });
+
+  describe('Unix time input', () => {
+    it('should accept negative Unix time', (done) => {
+      chai.request(server)
+        .get('/-100')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('object');
+          res.body.should.have.own.property('unix').eql(-100);
+          done();
+        });
+    });
+
+    it('should truncate fractional Unix time', (done) => {
+      chai.request(server)
+        .get('/1.5')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('object');
+          res.body.should.have.own.property('unix').eql(1);
+          done();
+        });
+    });
+  });
+
+  describe('Natural date input', () => {
+    const test = (done) => (err, res) => {
+      res.should.have.status(200);
+      res.body.should.be.an('object');
+      res.body.should.have.own.property('unix').eql(0);
+      res.body.should.have.own.property('natural').eql('January 1, 1970');
+      done();
+    };
+
+    it('should accept <Month Day Year> format', (done) => {
+      chai.request(server)
+        .get('/January 1 1970')
+        .end(test(done));
+    });
+
+    it('should accept <Day Month Year> format', (done) => {
+      chai.request(server)
+        .get('/1 January 1970')
+        .end(test(done));
+    });
+
+    it('should accept <Year Month Day> format', (done) => {
+      chai.request(server)
+        .get('/1970 January 1')
+        .end(test(done));
+    });
+  });
+
+  describe('Invalid input', () => {
+    it('should treat non-date formats as invalid', (done) => {
+      chai.request(server)
+        .get('/a')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('object');
+          res.body.should.have.own.property('unix').to.be.null;
+          res.body.should.have.own.property('natural').to.be.null;
+          done();
+        });
+    });
+
+    it('should treat unsafe integers as invalid', (done) => {
+      chai.request(server)
+        .get('/9007199254740992')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('object');
+          res.body.should.have.own.property('unix').to.be.null;
+          res.body.should.have.own.property('natural').to.be.null;
+          done();
+        });
+    });
+  });
 });
