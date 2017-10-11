d7c3bb1b8072cd85a601c46e44867b5064696ae2
Kev Comedia <kevcomedia@gmail.com>
2017-10-11 13:08:09 +0800

    Write JSDoc for the timestamp functions

--- app/routes/timestamp.js
+++ app/routes/timestamp.js
@@ -11,6 +11,16 @@ const dateFormats = [
   'Y MMM D',
 ];
 
+/**
+ * Returns an object that contains a Unix time and its corresponding natural
+ * date from the provided moment object. If there is none provided, the object
+ * will have `null` for both properties.
+ *
+ * @param {object} m The moment object to get the Unix time and natural date
+ * from
+ * @return {object} An object that contains a Unix time and its corresponding
+ * natural date. If `m` is null, both values are also null.
+ */
 function createTimestampObject(m = null) {
   if (m) {
     return {
@@ -25,6 +35,14 @@ function createTimestampObject(m = null) {
   }
 }
 
+/**
+ * Responds with a JSON that contains a Unix time and its corresponding natural
+ * date from the request parameter. If the date input is malformed, the JSON
+ * will contain null for both values.
+ *
+ * @param {object} req The request object
+ * @param {object} res The response object
+ */
 function getTimestamp(req, res) {
   const {date} = req.params;
   let timestamp;
