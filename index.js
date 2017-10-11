66a5f9a4e79496328d55a137da45ceea99484f33
Kev Comedia <kevcomedia@gmail.com>
2017-10-11 14:31:27 +0800

    Include recommended eslint rules

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, function() {
-  console.log('App is alive at port ' + port);
-});
+app.listen(port);
--- test/timestamp.js
+++ test/timestamp.js
@@ -1,3 +1,5 @@
+/* eslint-env mocha */
+
 process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
